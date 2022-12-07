ae8074e83d99e2c67da2b7fb97a91c1485b256c8
Christopher Phillips <112596258+ChristopherPhillips9@users.noreply.github.com>
2022-12-07 16:54:30 -0500

    Update gamePlayer.py. Write logic for what happens if a pawn guesses incorrectly inside of playingBoard().
    playingBoard() returns number of remaining players along with the current bridge. playingBoard prints the value of the board the game is played.

--- gamePlayer.py
+++ gamePlayer.py
@@ -6,31 +6,65 @@ __author__ = "Christopher Phillips, christopher.phillips9@snhu.edu"
 
 # pawn = the person who will be trying to make it across
 
+from random import randint
 from bridgeGenerator import bridgeGenerator
 
+def pawnGuess(current):
+    # Grab the current segment from the game.
+    # Remove the instances of X that occur, make a guess based on the remaining values available
 
-def pawnGuess():
-    pass
+    return currentSegment
 
 
 def playingBoard(players, bridge):
     # Define the number of players remaining
     remainingPlayers = players
 
-    print(bridge)
-    pass
+    # Iterates through all the segments of the bridge. currentSegment is incremented when a correct tile guess is made.
+    currentSegment = 0
+    while currentSegment in range(len(bridge)):
+
+        # Takes a random guess based on the number of tiles in the segment.
+        # If there are 2 segments, it will return 0 or 1
+        # TODO: make it guess more effectively and send to pawnGuess():
+        tileGuess = randint(0, (len(bridge[currentSegment]) - 1))
+
+        # Test to see how the game is playing and making sure its working correctly
+        print("Remaining Players:", str(remainingPlayers), "bridge:", str(bridge))
+
+        # If the guess is incorrect:
+        # The remainingPlayers count decreases and currentSegment is not increased
+        # The tile is replaced with X. There will be no consequences if tileGuess chooses this tile.
+        if bridge[currentSegment][tileGuess] == 1:
+            remainingPlayers = remainingPlayers - 1
+            bridge[currentSegment][tileGuess] = "X"
+            if remainingPlayers == 0:
+                return 0
+        # If the guess is correct:
+        # The currentSegment increases. The tile is replaced with C. This helps visualize the game
+        elif bridge[currentSegment][tileGuess] == 0:
+            bridge[currentSegment][tileGuess] = "C"
+            currentSegment = currentSegment + 1
+            if currentSegment >= len(bridge):
+                return remainingPlayers
 
 
 def playGame(players, segments, tiles, incorrect):
     # Generates a bridge for the game to be played on
     bridge = bridgeGenerator(segments, tiles, incorrect)
+    # bridge[0] = [2, 3]
 
     # Play the game
-    playingBoard(players, bridge)
+    results = playingBoard(players, bridge)
 
-    pass
+    return results
 
 
 # Example of how to use this code:
 # playGame(Number of players, number of segments, number of tiles per segment, number of incorrect tiles per segment)
-playGame(20, 18, 2, 1)
+gameResults = []
+
+#for i in range(1000):
+#    gameResults[i] = playGame(20, 18, 2, 1)
+
+print(playGame(20, 18, 2, 1))
