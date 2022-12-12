d304e82eaa1d3484cd553f5080ae284d89f01cf6
Christopher Phillips <112596258+ChristopherPhillips9@users.noreply.github.com>
2022-12-11 22:34:12 -0500

    Create bridgeObfuscator
    Create function that takes given bridge and makes a copy filled with "?"

--- bridgeRenderer.py
+++ bridgeRenderer.py
@@ -1,3 +1,27 @@
 #!/usr/bin/env python
 __author__ = "Christopher Phillips, christopher.phillips9@snhu.edu"
 
+# Takes a bridge generated from bridgeGenerator and makes it display friendly for players.
+
+from copy import deepcopy
+from bridgeGenerator import bridgeGenerator
+
+
+def bridgeObfuscator(bridge):
+    # Makes a copy of the bridge that will be filled with '?' instead of anything else so that the player cannot see.
+
+    # deepcopy is used to make an actual copy as opposed to a reference
+    displayBridge = deepcopy(bridge)
+
+    # Writes "?" for every element in the original array.
+    for i in range(len(displayBridge)):
+        for j in range(len(displayBridge[i])):
+            displayBridge[i][j] = "?"
+
+    return displayBridge
+
+
+def bridgeWindow(bridge):
+    pass
+
+
