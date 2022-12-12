f1b6e9fa9d9985453e03510872687b9ea959a2ff
Christopher Phillips <112596258+ChristopherPhillips9@users.noreply.github.com>
2022-12-11 23:04:40 -0500

    Create bridgePrint
    Make bridgePrint display any given bridge with instructions and info

--- bridgeRenderer.py
+++ bridgeRenderer.py
@@ -16,12 +16,38 @@ def bridgeObfuscator(bridge):
     # Writes "?" for every element in the original array.
     for i in range(len(displayBridge)):
         for j in range(len(displayBridge[i])):
-            displayBridge[i][j] = "?"
+            displayBridge[i][j] = "█"
 
     return displayBridge
 
 
-def bridgeWindow(bridge):
-    pass
+def bridgePrint(bridge, players, odds):
+    # This is the window that the player sees. It also prints instructions
+    # This function returns the choice the player made
+
+    # formatting and header
+    print("\n\n")
+    print("Key: █ = unknown tile, X = incorrect tile, C = correct guess")
+    print("\nplayers remaining:", players)
+    print("\nChances of the current player surviving:", math)
+    print("\nAverage survival amount for this bridge:", odds)
+
+    # print the bridge
+    for i in range(len(bridge)):
+
+        # Turn the current section into a string
+        displayBridgeSegment = str(bridge[i])
+
+        # Strip away the brackets and commas from displayBridgeSegment
+        displayBridgeSegment = displayBridgeSegment.strip("[").strip("]").strip("'").replace(',', '').replace('\'', '')
+
+        # Print out the edited string so that it's good-looking
+        # Formatting to add number padding. It works up to 99.
+        if i < 10:
+            print("segment 0" + str(i) + ": " + displayBridgeSegment)
+        elif i < 100:
+            print("segment " + str(i) + ": " + displayBridgeSegment)
 
 
+
+bridgePrint(bridgeGenerator(18, 3, 1))
