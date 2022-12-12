431f5830eafc3b4f372492e0e56ddcdf1cb1f8d3
Christopher Phillips <112596258+ChristopherPhillips9@users.noreply.github.com>
2022-12-11 20:28:16 -0500

    Create new gamePlayer
    new gamePlayer allows player to play game manually without random guessing

---
+++ gamePlayer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+__author__ = "Christopher Phillips, christopher.phillips9@snhu.edu"
+
+# This allows the user to play through the game.
+# The user starts with however many specified players that they have and if the player count reaches 0, they lose.
+# If the player plays multiple times than the average amount of survivors will be printed out
+
+def gamePlayer():
